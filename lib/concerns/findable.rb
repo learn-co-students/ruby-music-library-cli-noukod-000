553b1f1bb9651054800fc78433a5b5cca9f93b0a
Poliard Djimmy <poliarddjimmy@gmail.com>
2019-05-06 14:35:03 +0000

    Done.

--- lib/concerns/findable.rb
+++ lib/concerns/findable.rb
@@ -1,14 +1,12 @@
 module Concerns::Findable
 
 # Allow to find an instance of the class by name.
-  def find_by_name(f_name)
-     all.detect do |song_dectect|
-       song_dectect.name == f_name
+  def find_by_name(name)
+     all.find do |song_dectect|
+       song_dectect.name == name
      end
   end
 
-# Allow to find an instance of the class by name and
-# to create a new instance if a match is not found.
   def find_or_create_by_name(name) #
       self.find_by_name(name) || self.create(name)
   end
